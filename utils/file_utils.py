ea0442bbf923413e5218c9031f5fd1333c5db389
Gokul NC <gokulnc@ymail.com>
2020-05-07 15:28:52 +0000

    Add util to get pathnames with size < 255

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,4 +1,6 @@
 import json
+import sys
+import os
 
 def pretty_write_json(data, outfile, sort_keys=False):
     with open(outfile, 'w', encoding='utf-8') as f:
@@ -8,3 +10,13 @@ INVALID_FILENAME_CHARS = '<>:"/\\|?*'
 def get_valid_filename(filename):
     # Note: Tested only on Windows
     return ''.join(c for c in filename if c not in INVALID_FILENAME_CHARS)
+
+MAX_PATH_LENGTH_BYTES = 255
+def get_verified_path(folder, filename, extension_with_dot):
+    # assert sys.getsizeof(folder) + sys.getsizeof(extension_with_dot) < MAX_PATH_LENGTH_BYTES
+    filename = get_valid_filename(filename)
+    pathname = os.path.abspath(os.path.join(folder, filename))
+    max_pathname_bytes = MAX_PATH_LENGTH_BYTES - sys.getsizeof(extension_with_dot)
+    if sys.getsizeof(pathname) > max_pathname_bytes:
+        pathname = bytearray(pathname.encode('utf8'))[:max_pathname_bytes].decode(errors='ignore')
+    return pathname + extension_with_dot
